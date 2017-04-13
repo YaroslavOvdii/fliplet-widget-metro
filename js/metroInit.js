165ef93ebf31428e3216a9fc2980b7bd89cc4ada
Hugo Carneiro <hcarneiro@weboo.biz>
2017-04-13 17:14:49 +0100

    Improved grid size to support all device sizes

--- js/metroInit.js
+++ js/metroInit.js
@@ -1,94 +1,99 @@
 UIFreewallVertical = (function() {
 
-	var data;
+  var data;
 
-	function UIFreewallVertical(config) {
-		data = config;
-		UIFreewallVertical.setupFreewalVertical(data);
-		window.addEventListener( 'resize', UIFreewallVertical.setupFreewalVertical(data), false );
-	}
+  function UIFreewallVertical(config) {
+    data = config;
+    UIFreewallVertical.setupFreewalVertical(data);
+    window.addEventListener('resize', UIFreewallVertical.setupFreewalVertical(data), false);
+  }
 
-	UIFreewallVertical.loadMetro = function() {
-		$('[data-metro-id="'+data.id+'"]:not([data-mce-bogus] [data-metro-id="'+data.id+'"])').addClass('ready');
-	};
+  UIFreewallVertical.loadMetro = function() {
+    $('[data-metro-id="' + data.id + '"]:not([data-mce-bogus] [data-metro-id="' + data.id + '"])').addClass('ready');
+  };
 
-	UIFreewallVertical.setupFreewalVertical = function(data) {
-		var wall = new freewall('[data-metro-id="'+data.id+'"]:not([data-mce-bogus] [data-metro-id="'+data.id+'"])');
+  UIFreewallVertical.setupFreewalVertical = function(data) {
+    var wall = new freewall('[data-metro-id="' + data.id + '"]:not([data-mce-bogus] [data-metro-id="' + data.id + '"])');
     var cellWidth;
     var cellHeight;
-		wall.reset({
-			selector : '.panels',
-			cellW : function() {
+    wall.reset({
+      selector: '.panels',
+      cellW: function() {
 
-				var width = $('body').width();
+        var width = $('body').width();
 
-				if (width >= 1300) {
-					cellWidth = 330;
-					return cellWidth;
-				} else if (width >= 1200) {
-					cellWidth = 310;
-					return cellWidth;
-				} else if (width >= 1024) {
-					cellWidth = 230;
-					return cellWidth;
-				} else if (width >= 640) {
-					cellWidth = 185;
-					return cellWidth;
-				} else if (width >= 400) {
-					cellWidth = 150;
-					return cellWidth;
-				} else {
-					cellWidth = 135;
-					return cellWidth;
-				}
-		  },
-			cellH : function() {
+        if (width >= 1300) {
+          cellWidth = 330;
+          return cellWidth;
+        } else if (width >= 1200) {
+          cellWidth = 310;
+          return cellWidth;
+        } else if (width >= 1024) {
+          cellWidth = 230;
+          return cellWidth;
+        } else if (width >= 640) {
+          cellWidth = 185;
+          return cellWidth;
+        } else if (width >= 400) {
+          cellWidth = 155;
+          return cellWidth;
+        } else {
+          cellWidth = 140;
+          return cellWidth;
+        }
+      },
+      cellH: function() {
 
-				var width = $('body').width();
+        var width = $('body').width();
 
-				if (width >= 1300) {
-					cellHeight = 330;
-					return cellHeight;
-				} else if (width >= 1200) {
-					cellHeight = 310;
-					return cellHeight;
-				} else if (width >= 1014) {
-					cellHeight = 230;
-					return cellHeight;
-				} else if (width >= 640) {
-					cellHeight = 185;
-					return cellHeight;
-				} else if (width >= 400) {
-					cellHeight = 150;
-					return cellHeight;
-				} else {
-					cellHeight = 135;
-					return cellHeight;
-				}
-		  },
-			gutterY : data.enableGap ? 10 : 0,
-			gutterX : data.enableGap ? 10 : 0,
+        if (width >= 1300) {
+          cellHeight = 330;
+          return cellHeight;
+        } else if (width >= 1200) {
+          cellHeight = 310;
+          return cellHeight;
+        } else if (width >= 1014) {
+          cellHeight = 230;
+          return cellHeight;
+        } else if (width >= 640) {
+          cellHeight = 185;
+          return cellHeight;
+        } else if (width >= 400) {
+          cellHeight = 155;
+          return cellHeight;
+        } else {
+          cellHeight = 140;
+          return cellHeight;
+        }
+      },
+      gutterY: data.enableGap ? 10 : 0,
+      gutterX: data.enableGap ? 10 : 0,
       onResize: function() {
         wall.fitWidth();
-				wall.refresh();
+        wall.refresh();
+        location.reload();
       }
-		});
+    });
 
-		wall.fitWidth();
-		wall.refresh();
+    wall.fitWidth();
+    wall.refresh();
 
-		function addClass() {
-			if (Modernizr.android) {
-				setTimeout(function(){
-					$('div:not("[data-mce-bogus]") .metro-panels').addClass('ready');
-					setTimeout(function(){
-						$('body').css({ transform: "translateZ(1px)" }).css({ transform: "translateZ(0)" });
-					}, 300 );
-				}, 500 );
-			}
-		}
-	};
+    function addClass() {
+      if (Modernizr.android) {
+        setTimeout(function() {
+          $('div:not("[data-mce-bogus]") .metro-panels').addClass('ready');
+          setTimeout(function() {
+            $('body').css({
+              transform: "translateZ(1px)"
+            }).css({
+              transform: "translateZ(0)"
+            });
+          }, 300);
+        }, 500);
+      }
+    }
+  };
 
-	return UIFreewallVertical;
+  return UIFreewallVertical;
 
 })();
