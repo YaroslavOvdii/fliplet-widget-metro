bd2456f63b9251b0b1a02ba7bfc9baf56694f1bc
YaroslavOvdii <yaroslav.o@upplabs.com>
2019-11-12 15:12:00 +0200

    reset save button label

--- js/interface.js
+++ js/interface.js
@@ -294,6 +294,7 @@ function initImageProvider(item) {
         $('[data-id="' + item.id + '"] .add-image-holder').find('.thumb-holder').addClass('hidden');
       }
 
+      Fliplet.Widget.resetSaveButtonLabel();
       imageProvider = null;
     }
   });
